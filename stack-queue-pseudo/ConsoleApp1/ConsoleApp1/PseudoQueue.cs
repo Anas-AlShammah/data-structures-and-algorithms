ed14c8828e7e15e18081f0affaad3f3a60060f65
anasgharaibeh <anas.gharaibeh1@yahoo.com>
2023-07-16 22:28:32 +0000

    stack-queue-pseudo

---
+++ stack-queue-pseudo/ConsoleApp1/ConsoleApp1/PseudoQueue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class PseudoQueue : IQueue
+    {
+        private Stack inbox;
+        private Stack outbox;
+        public PseudoQueue()
+        {
+            inbox = new Stack();
+            outbox = new Stack();
+        }
+        public int Dequeue()
+        {
+            if (outbox.IsEmpty)
+            {
+                while (!inbox.IsEmpty)
+                {
+                    outbox.Push(inbox.Pop());
+                }
+            }
+
+            return outbox.Pop();
+        }
+
+        public void Enqueue(int value)
+        {
+            inbox.Push(value);
+        }
+    }
+}
