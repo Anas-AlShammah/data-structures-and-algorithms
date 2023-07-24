5ed251132bd451cd39e3ad69b673e6cd070f8d51
anasgharaibeh <anas.gharaibeh1@yahoo.com>
2023-07-24 00:28:44 +0000

    edit Test

---
+++ stack-queue-brackets/ConsoleApp1/ConsoleApp1/Brackets.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Brackets
+    {
+        public bool ValidateBrackets(string str)
+        {
+            string str1 = "({[";
+            string str2 = ")}]";
+            List<int> list1 = new List<int>();
+            List<int> list2 = new List<int>();
+            foreach (char c in str)
+            {
+                if (str1.Contains(c))
+                {
+                    list1.Add(str1.IndexOf(c));
+                }
+                else if (str2.Contains(c))
+                {
+                    list2.Add(str2.IndexOf(c));
+                }
+            }
+            if (list1.Count == list2.Count)
+            {
+                for (int i = 0; i < list1.Count; i++)
+                {
+                    if (list1[i] != list2[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
