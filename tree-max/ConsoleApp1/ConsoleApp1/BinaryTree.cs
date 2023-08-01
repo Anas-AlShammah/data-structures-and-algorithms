0133e83a888de3cd0dc6d63c48e8db2b69895216
anasgharaibeh <anas.gharaibeh1@yahoo.com>
2023-08-01 10:27:42 +0000

    tree-max

---
+++ tree-max/ConsoleApp1/ConsoleApp1/BinaryTree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class BinaryTree
+    {
+        public Node root { get; set; }
+        public  int FindMax(Node node)
+        {
+            if (node == null) return int.MinValue;
+            int res = node.Data;
+            int lres = FindMax(node.Left);
+            int rres = FindMax(node.Right);
+            if (lres > res)
+            {
+                res = lres;
+            }
+            if (rres > res)
+            {
+                res = rres;
+            }
+            return res;
+        }
+    }
+}
