90e6dca4ce8d8dc9c77b4041ba47361313685e43
Anas Al Shammah <anas.gharaibeh1@yahoo.com>
2023-09-09 23:58:18 +0300

    sort-object

---
+++ Sort-Object/ConsoleApp1/ConsoleApp1/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public List<string> Genres { get; set; }
+    }
+}
---
+++ Sort-Object/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,82 @@
+﻿using ConsoleApp1;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var movie1 = new Movie
+        {
+            Title = "The Shawshank Redemption",
+            Year = 1999,
+            Genres = new List<string> { "Drama", "Crime" }
+        };
+        var movie2 = new Movie
+        {
+            Title = "The Godfather",
+            Year = 1972,
+            Genres = new List<string> { "Drama", "Crime" }
+        };
+        var movie3 = new Movie
+        {
+            Title = "Pulp Fiction",
+            Year = 1994,
+            Genres = new List<string> { "Crime", "Drama" }
+        };
+        var movieList = new List<Movie> { movie1, movie2, movie3 };
+        var newList = SortByYear(movieList);
+        Console.WriteLine("Sorted Movie Data by Year:");
+        foreach (var movie in newList)
+        {
+            Console.WriteLine($"Title: {movie.Title}");
+            Console.WriteLine($"Year: {movie.Year}");
+            Console.WriteLine("Genres:");
+            foreach (var genre in movie.Genres)
+            {
+                Console.WriteLine($"- {genre}");
+            }
+            Console.WriteLine();
+        }
+        var newList2 = SortByTitle(movieList);
+        Console.WriteLine("Sorted Movie Data by Title:");
+        foreach (var movie in newList2)
+        {
+            Console.WriteLine($"Title: {movie.Title}");
+            Console.WriteLine($"Year: {movie.Year}");
+            Console.WriteLine("Genres:");
+            foreach (var genre in movie.Genres)
+            {
+                Console.WriteLine($"- {genre}");
+            }
+            Console.WriteLine();
+        }
+    }
+    public static List<Movie> SortByYear(List<Movie> movies)
+    {
+        List<Movie> result = movies.OrderByDescending(movie => movie.Year).ToList();
+        return result;
+    }
+    public static List<Movie> SortByTitle(List<Movie> movies)
+    {
+
+        List<Movie> result = movies
+            .OrderBy(movie =>
+            {
+                string title = movie.Title;
+                if (title.StartsWith("The ", StringComparison.OrdinalIgnoreCase))
+                {
+                    title = title.Substring(4);
+                }
+                else if (title.StartsWith("A ", StringComparison.OrdinalIgnoreCase))
+                {
+                    title = title.Substring(2);
+                }
+                else if (title.StartsWith("An ", StringComparison.OrdinalIgnoreCase))
+                {
+                    title = title.Substring(3);
+                }
+                return title;
+            })
+            .ToList();
+        return result;
+    }
+}
