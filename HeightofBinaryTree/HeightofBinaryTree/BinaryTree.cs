958532d516f734476b3017fcd59a190ef31cf91b
Anas Al Shammah <anas.gharaibeh1@yahoo.com>
2023-10-29 12:36:29 +0300

    HeightofBinaryTree

---
+++ HeightofBinaryTree/HeightofBinaryTree/BinaryTree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeightofBinaryTree
+{
+    public class BinaryTree
+    {
+       public Node Root { get; set; }
+       public int MaxDepth(Node node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+            else
+            {
+                int LDepth = MaxDepth(node.Left);
+                int RDepth = MaxDepth(node.Right);
+                return Math.Max(LDepth, RDepth) ;
+            }
+        }
+    }
+
+}
---
+++ HeightofBinaryTree/HeightofBinaryTree/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using HeightofBinaryTree;
+
+BinaryTree tree = new BinaryTree();
+
+tree.Root = new Node(1);
+tree.Root.Left = new Node(2);
+tree.Root.Right = new Node(3);
+tree.Root.Left.Left = new Node(4);
+tree.Root.Left.Right = new Node(5);
+
+Console.WriteLine("Height of tree is "
+                  + tree.MaxDepth(tree.Root));
